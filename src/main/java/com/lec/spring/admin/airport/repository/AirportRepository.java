8a7a34ecd9e6f8f958ca37eb9c51944b15a89b28
GayeonHong726 <1379hong@naver.com>
2026-02-06 19:11:50 +0900

    [26.2.6] AirportRepository

--- src/main/java/com/lec/spring/admin/airport/repository/AirportRepository.java
+++ src/main/java/com/lec/spring/admin/airport/repository/AirportRepository.java
@@ -3,6 +3,12 @@ package com.lec.spring.admin.airport.repository;
 import com.lec.spring.admin.airport.domain.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public class AirportRepository extends JpaRepository<Airport, Long> {
 
+    @Override
+    public List<Airport> findAllById(Iterable<Long> longs) {
+        return List.of();
+    }
 }
