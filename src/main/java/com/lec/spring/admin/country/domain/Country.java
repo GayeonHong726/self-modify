a9bffd5e35ae959917f40f1df58354b96b3ceba9
GayeonHong726 <1379hong@naver.com>
2026-02-11 19:18:32 +0900

    [26.2.11] Country 엔티티

--- src/main/java/com/lec/spring/admin/country/domain/Country.java
+++ src/main/java/com/lec/spring/admin/country/domain/Country.java
@@ -3,14 +3,11 @@ package com.lec.spring.admin.country.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.lec.spring.admin.airport.domain.Airport;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -22,15 +19,16 @@ public class Country {
     private Long id;
 
     @Column(name = "country_id")
-    private Integer countryId; // 나라 id : countryId
+    private Integer countryId;
 
-    @Column(unique = true, name = "country_iso")
-    private String countryIso; // 나라 ISO 코드 : codeIso2Country
+    @Column(unique = true, name = "country_iso", nullable = false)
+    private String countryIso;
 
-    @Column(name = "country_name")
-    private String countryName; // 나라 이름 : nameCountry
+    @Column(name = "country_name", nullable = false)
+    private String countryName;
 
     @OneToMany(mappedBy = "country", cascade = CascadeType.REMOVE, orphanRemoval = true)
     @JsonIgnore
+    @ToString.Exclude
     private List<Airport> airports;
 }
